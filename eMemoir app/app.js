2ab3a34bf04b8321ec97d81588b2e6e3cd5d8d9f
mohitraj2741 <mohitraj2741@gmail.com>
2022-07-18 16:43:43 +0530

    f

--- eMemoir app/app.js
+++ eMemoir app/app.js
@@ -32,4 +32,4 @@ app.use(require("./routes/memo"))
 
 //server configurations
 
-app.listen(3000||process.env.PORT,()=> console.log("server started at port 3000 "))
+app.listen(process.env.PORT || 3000,()=> console.log("server started at port 3000 "))
